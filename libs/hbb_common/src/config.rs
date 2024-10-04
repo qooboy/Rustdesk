957f4e27d98e915bebfdc0a4ccb6e96df307cc0c
qooboy <szdyf@qq.com>
2024-10-04 14:39:52 +0700

    修改cn

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["cn.e-123.cn"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["cn2.e-123.cn"];
 pub const PUBLIC_RS_PUB_KEY: &str = "kpJ9To6+2cVx5nAFB7z73fCGhPh4GBLtOAUdf2Rscsg=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
